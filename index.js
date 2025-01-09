7cf561b25b05eea44dddd1e3a79e4a795d56de4a
p0int088 <la1mon2789@gmail.com>
2025-01-09 14:54:14 +1000

    страница чмо

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-alert(
-    "pidor"
-)
+const btn = document.getElementById("1");
+const audio = new Audio("koroki--luchshiy-zvuk-na-donat.mp3");
+
+btn.addEventListener("mouseenter", () => {
+  btn.innerText = "ХААХААХААХАХААХАХ СОСАЛ?! ДА. ЧУХАН";
+
+  audio.currentTime = 0;
+
+  audio.play();
+});
+
+btn.addEventListener("mouseleave", () => {
+  audio.pause();
+  btn.innerText = "КНОПКА";
+});
